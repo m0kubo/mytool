5bd7acf8f5f050f91053a94e455459405a13b335
M.Okubo <masahiko.okubo@insprout.com>
2018-03-30 16:52:06 +0900

    Camera制御用クラスを独立

---
+++ app/src/main/java/com/insprout/okubo/mytool/CameraUi.java
@@ -0,0 +1,148 @@
+package com.insprout.okubo.mytool;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.IOException;
+import java.util.List;
+
+public class CameraUi implements SurfaceHolder.Callback {
+
+    private Context mContext;
+    private SurfaceView mSurfaceView;
+    private SurfaceHolder mSurfaceHolder;
+    private Camera mCamera;
+    private boolean mFocusing = false;
+
+
+    @SuppressLint("ClickableViewAccessibility")
+    public CameraUi(Context context, SurfaceView surfaceView) {
+        mContext = context;
+        mSurfaceView = surfaceView;
+        mSurfaceHolder = mSurfaceView.getHolder();
+        mSurfaceHolder.addCallback(this);
+        mSurfaceView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (mFocusing) return true;
+                if (motionEvent.getActionMasked() == MotionEvent.ACTION_UP) {
+                    autoFocus();
+                }
+                return true;
+            }
+        });
+    }
+
+    public void close() {
+        if (mCamera!= null) {
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    private void autoFocus() {
+        if (mCamera == null) return;
+
+        mFocusing = true;
+        mCamera.autoFocus(new Camera.AutoFocusCallback() {
+            @Override
+            public void onAutoFocus(boolean b, Camera camera) {
+                mFocusing = false;
+            }
+        });
+    }
+
+
+    // 最も横長な プレビューサイズを選らぶ
+    private Camera.Size getWidePreviewSize(List<Camera.Size> sizes) {
+        if (sizes == null) {
+            return null;
+        }
+
+        Camera.Size sizeWide = null;
+        double ratioMax = 0;
+        for (Camera.Size size : sizes) {
+            if (size.height <= 0) continue;
+            double ratio = (double) size.width / size.height;
+            if (ratio >= ratioMax) {
+                ratioMax = ratio;
+                sizeWide = size;
+            }
+        }
+
+        return sizeWide;
+    }
+
+
+    //////////////////////////////////////////////////////////////////////
+    //
+    // SurfaceHolder.Callback 実装
+    //
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        mCamera = Camera.open();
+        try {
+            mCamera.setPreviewDisplay(mSurfaceHolder);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
+        mCamera.stopPreview();
+
+        Camera.Parameters parameters = mCamera.getParameters();
+        // 画面の向きを設定
+        int orientation = mContext.getResources().getConfiguration().orientation;
+        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+            mCamera.setDisplayOrientation(90);
+        } else {
+            mCamera.setDisplayOrientation(0);
+        }
+
+        // 対応するプレビューサイズを決定する
+        Camera.Size previewSize = getWidePreviewSize(parameters.getSupportedPreviewSizes());
+        ViewGroup.LayoutParams layoutParams = mSurfaceView.getLayoutParams();
+        double preview_ratio = (double)previewSize.width / (double)previewSize.height;
+        if (width > height) {
+            // 横長
+            int new_height = (int)(width / preview_ratio);
+            if (new_height <= height) {
+                layoutParams.height = height;
+            } else {
+                layoutParams.width = (int)(height * preview_ratio);
+            }
+
+        } else {
+            // 縦長
+            int new_width = (int)(height / preview_ratio);
+            if (new_width <= width) {
+                layoutParams.width = new_width;
+            } else {
+                layoutParams.height = (int)(width * preview_ratio);
+            }
+        }
+        mSurfaceView.setLayoutParams(layoutParams);
+
+        // パラメータを設定してカメラを再開
+        mCamera.setParameters(parameters);
+        mCamera.startPreview();
+        autoFocus();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        close();
+    }
+
+}
--- app/src/main/java/com/insprout/okubo/mytool/HorizonMeterActivity.java
+++ app/src/main/java/com/insprout/okubo/mytool/HorizonMeterActivity.java
@@ -22,17 +22,14 @@ import android.widget.Toast;
 import java.io.IOException;
 import java.util.List;
 
-public class HorizonMeterActivity extends AppCompatActivity implements SensorEventListener, SurfaceHolder.Callback {
+public class HorizonMeterActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager mSensorManager;
     private Sensor mMagneticSensor = null;          // 磁気センサー
     private Sensor mAccelerometerSensor = null;     // 加速度センサー
     private TextView mTvHorizon, mTvFace;
 
-    private SurfaceView mSurfaceView;
-    private SurfaceHolder mSurfaceHolder;
-    private Camera mCamera;
-    private boolean mFocusing = false;
+    private CameraUi mCameraUi;
 
 
     @Override
@@ -88,31 +85,8 @@ public class HorizonMeterActivity extends AppCompatActivity implements SensorEve
         mTvHorizon = findViewById(R.id.tv_angle);
         mTvFace = findViewById(R.id.tv_face);
 
-        mSurfaceView = findViewById(R.id.surfaceView);
-        mSurfaceHolder = mSurfaceView.getHolder();
-        mSurfaceHolder.addCallback(this);
-        mSurfaceView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (mFocusing) return true;
-                if (motionEvent.getActionMasked() == MotionEvent.ACTION_UP) {
-                    autoFocus();
-                }
-                return true;
-            }
-        });
-    }
-
-    public void autoFocus() {
-        if (mCamera == null) return;
-
-        mFocusing = true;
-        mCamera.autoFocus(new Camera.AutoFocusCallback() {
-            @Override
-            public void onAutoFocus(boolean b, Camera camera) {
-                mFocusing = false;
-            }
-        });
+        SurfaceView surfaceView = findViewById(R.id.surfaceView);
+        mCameraUi = new CameraUi(this, surfaceView);
     }
 
 
@@ -190,95 +164,6 @@ public class HorizonMeterActivity extends AppCompatActivity implements SensorEve
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
-    }
-
-
-
-    //////////////////////////////////////////////////////////////////////
-    //
-    // SurfaceHolder.Callback 実装
-    //
-
-    @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        mCamera = Camera.open();
-        try {
-            mCamera.setPreviewDisplay(mSurfaceHolder);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int format, int width, int height) {
-        mCamera.stopPreview();
-
-        Camera.Parameters parameters = mCamera.getParameters();
-        // 画面の向きを設定
-        int orientation = getResources().getConfiguration().orientation;
-        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-            mCamera.setDisplayOrientation(90);
-        } else {
-            mCamera.setDisplayOrientation(0);
-        }
-
-        // 対応するプレビューサイズを決定する
-        Camera.Size previewSize = getWidePreviewSize(parameters.getSupportedPreviewSizes());
-        ViewGroup.LayoutParams layoutParams = mSurfaceView.getLayoutParams();
-        double preview_ratio = (double)previewSize.width / (double)previewSize.height;
-        if (width > height) {
-            // 横長
-            int new_height = (int)(width / preview_ratio);
-            if (new_height <= height) {
-                layoutParams.height = height;
-            } else {
-                layoutParams.width = (int)(height * preview_ratio);
-            }
-
-        } else {
-            // 縦長
-            int new_width = (int)(height / preview_ratio);
-            if (new_width <= width) {
-                layoutParams.width = new_width;
-            } else {
-                layoutParams.height = (int)(width * preview_ratio);
-            }
-        }
-        mSurfaceView.setLayoutParams(layoutParams);
-
-        // パラメータを設定してカメラを再開
-        mCamera.setParameters(parameters);
-        mCamera.startPreview();
-        //mCamera.autoFocus(null);
-        autoFocus();
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        mCamera.stopPreview();
-        mCamera.release();
-        mCamera = null;
-    }
-
-
-    // 最も横長な プレビューサイズを選らぶ
-    private Camera.Size getWidePreviewSize(List<Camera.Size> sizes) {
-        if (sizes == null) {
-            return null;
-        }
-
-        Camera.Size sizeWide = null;
-        double ratioMax = 0;
-        for (Camera.Size size : sizes) {
-            if (size.height <= 0) continue;
-            double ratio = (double) size.width / size.height;
-            if (ratio >= ratioMax) {
-                ratioMax = ratio;
-                sizeWide = size;
-            }
-        }
-
-        return sizeWide;
     }
 
 
